a751e55a268915dd36776ef3c31f8ddc704be122
tores77 <tu@email.com>
2026-06-30 23:25:32 +0200

    fix: metatag SEObox en root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "TXZ6d0Fld25Oa2pySW13c005anpZaD1x": "5e6ce366e163ff1f5e177e7be7f7a94c",
+  },
+};
 
 export default function RootLayout({
   children,
